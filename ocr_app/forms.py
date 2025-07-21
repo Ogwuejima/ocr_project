a55134b092e45a54498989729573d4aea5d9eea9
Ogwuejima <133676794+Ogwuejima@users.noreply.github.com>
2025-07-21 22:36:20 +0100

    Add files via upload

---
+++ ocr_app/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from .models import OCRResult
+
+class RegisterForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+class UploadImageForm(forms.ModelForm):
+    class Meta:
+        model = OCRResult
+        fields = ['image']
---
+++ ocr_app/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class OCRResult(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='uploads/')
+    extracted_text = models.TextField()
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def str(self):
+        return f"{self.user.username} - {self.uploaded_at}"
