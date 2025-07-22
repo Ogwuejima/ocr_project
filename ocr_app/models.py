39dbc6b51ca452de377d2ea89cbeb6d35a59374c
Suleiman Hassan <ogwuejima@gmail.comgit config --global user.name Suleiman>
2025-07-22 02:40:47 +0100

    Fix __str__ method in OCRResult model

--- ocr_app/models.py
+++ ocr_app/models.py
@@ -1,9 +1,5 @@
-
-
 from django.db import models
 from django.contrib.auth.models import User
-
-# Create your models here.
 
 class OCRResult(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -11,5 +7,5 @@ class OCRResult(models.Model):
     extracted_text = models.TextField()
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
-    def str(self):
+    def __str__(self):
         return f"{self.user.username} - {self.uploaded_at}"
